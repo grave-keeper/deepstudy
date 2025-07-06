5625f317f40941ef07b1cb111bacd23c31622b4c
Vivaan Yadav <gravesoulkeeper@gmail.com>
2025-07-06 19:34:13 +0530

    profile page added

---
+++ client/src/pages/profile/script.js
@@ -0,0 +1,14 @@
+fetch('../../component/navbar')
+    .then(navbar => navbar.text())
+    .then(navbar => document.getElementById('navbar').innerHTML = navbar)
+    .catch(error => console.error("Error while fetching navbar \n", error))
+
+fetch('../../component/userProfile')
+    .then(userProfile => userProfile.text())
+    .then(userProfile => document.getElementById('profile').innerHTML = userProfile)
+    .catch(error => console.error("Error while fetching userProfile \n", error))
+
+fetch('../../component/footer')
+    .then(footer => footer.text())
+    .then(footer => document.getElementById('footer').innerHTML = footer)
+    .catch(error => console.error("Error while fetching footer \n", error))
