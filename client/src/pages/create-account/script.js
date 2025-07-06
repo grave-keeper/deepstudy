3788f2dff6288da364f3f6751f8628f0e840d094
Vivaan Yadav <gravesoulkeeper@gmail.com>
2025-07-06 15:44:25 +0530

    create account page added

---
+++ client/src/pages/create-account/script.js
@@ -0,0 +1,4 @@
+fetch('../../component/createAccountForm')
+    .then(createAccountForm => createAccountForm.text())
+    .then(createAccountForm => document.getElementById('create-account').innerHTML = createAccountForm)
+    .catch(error => console.error("Error while fetching createAccountForm \n", error))
