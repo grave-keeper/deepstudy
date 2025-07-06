09522e53756518fb60f3764b31259fa8b6e39799
Vivaan Yadav <gravesoulkeeper@gmail.com>
2025-07-06 15:41:55 +0530

    signin page added

---
+++ client/src/pages/signin/script.js
@@ -0,0 +1,4 @@
+fetch('../../component/signinForm')
+    .then(signinForm => signinForm.text())
+    .then(signinForm => document.getElementById('sigin').innerHTML = signinForm)
+    .catch(error => console.error("Error while fetching sigin \n", error))
