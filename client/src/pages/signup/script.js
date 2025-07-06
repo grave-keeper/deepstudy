4213cad34ac553e1a19633ae48d03f2a346ff37f
Vivaan Yadav <gravesoulkeeper@gmail.com>
2025-07-06 15:42:42 +0530

    signup page added

---
+++ client/src/pages/signup/script.js
@@ -0,0 +1,4 @@
+fetch('../../component/signupForm')
+    .then(signupForm => signupForm.text())
+    .then(signupForm => document.getElementById('signup').innerHTML = signupForm)
+    .catch(error => console.error("Error while fetching signupForm \n", error))
