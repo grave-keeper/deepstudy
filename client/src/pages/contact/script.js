b4bfeec1e3805f7f49692e0883d1de4a60c400c2
Vivaan Yadav <gravesoulkeeper@gmail.com>
2025-07-06 17:06:10 +0530

    contact page added

---
+++ client/src/pages/contact/script.js
@@ -0,0 +1,15 @@
+fetch('../../component/navbar')
+    .then(navbar => navbar.text())
+    .then(navbar => document.getElementById('navbar').innerHTML = navbar)
+    .catch(error => console.error("Error while fetching navbar \n", error))
+
+fetch('../../component/feedback')
+    .then(feedback => feedback.text())
+    .then(feedback => document.getElementById('feedback').innerHTML = feedback)
+    .catch(error => console.error("Error while fetching feedback \n", error))
+
+fetch('../../component/footer')
+    .then(footer => footer.text())
+    .then(footer => document.getElementById('footer').innerHTML = footer)
+    .catch(error => console.error("Error while fetching footer \n", error))
+
