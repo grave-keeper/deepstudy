23c9e60ea608298935bd3ee8bde0915e40c78ccc
Vivaan Yadav <gravesoulkeeper@gmail.com>
2025-07-30 20:25:19 +0530

    feat: created JWT middleware

---
+++ server/src/middleware/jwt.middleware.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken'
+import { JWT_ACCESS_TOKEN_SECRET } from '../config/constants.js'
+
+export const authMiddleware = (req, res, next) => {
+    console.log('inside the middleware...')
+    const token = req.cookies
+
+    if (!token) return res.status(401).json({ error: 'Unauthorized user' })
+
+    try {
+        const decode = jwt.verify(token.accessToken, JWT_ACCESS_TOKEN_SECRET)
+        req.user = decode
+        next()
+    } catch (error) {
+        console.log('Error in authorized ')
+        return res
+            .status(401)
+            .clearCookie('accessToken')
+            .clearCookie('refreshToken')
+            .json({ error: 'Unauthorized user' })
+    }
+}
