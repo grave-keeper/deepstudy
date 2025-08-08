b6fe4cdb9c6ab730fc48ec51075d15f6a7a56d8c
Vivaan Yadav <gravesoulkeeper@gmail.com>
2025-08-09 00:03:45 +0530

    feat: toggle CTA visibility when user is logged in

--- client/src/components/content/script.js
+++ client/src/components/content/script.js
@@ -6,3 +6,14 @@ cta.querySelector('button').onclick = () => {
     const email = cta.querySelector('input[type="email"]').value
     handleRegisterEmail(email)
 }
+
+const ctaLoggedIn = document.querySelector('#hero-section > .hero-left > .cta > .cta-logged-in')
+if (localStorage.getItem('user')) {
+    cta.parentElement.querySelector('p:first-child').style.display = 'none'
+    cta.style.display = 'none'
+    ctaLoggedIn.style.display = 'flex'
+}
+
+ctaLoggedIn.querySelector('button').onclick = () => {
+    window.location.href = "../../pages/pyq/index.html"
+}
