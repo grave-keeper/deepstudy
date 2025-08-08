aa63c4bc4dbef2f4b6299f38e623c61bc789033c
Vivaan Yadav <gravesoulkeeper@gmail.com>
2025-08-08 22:56:53 +0530

    feat: add field validity check and password visibility toggle

--- client/src/components/signinForm/script.js
+++ client/src/components/signinForm/script.js
@@ -1,9 +1,26 @@
 import { handleGoogleSignUp, handleGitHubSignup } from '../../services/authService.js'
 import { handleSignIn } from '../../services/userService.js'
 
+const eyeState = document.querySelector('#sigin-credentials .password .eye')
+const passwordState = document.querySelector('#sigin-credentials .password input')
+
+eyeState.onclick = () => {
+    if (eyeState.src.includes('eye_close')) {
+        eyeState.src = '../../../assets/svg/eye_open.svg'
+        passwordState.type = 'text'
+        return
+    }
+    eyeState.src = '../../../assets/svg/eye_close.svg'
+    passwordState.type = 'password'
+}
+
 const form = document.querySelector('#sigin-credentials > form')
 form.querySelector('button[type="submit"]').onclick = (event) => {
     event.preventDefault()
+    if (!form.checkValidity()) {
+        form.reportValidity()
+        return
+    }
     const formData = new FormData(form)
     handleSignIn(formData)
 }
--- client/src/components/signupForm/script.js
+++ client/src/components/signupForm/script.js
@@ -1,4 +1,16 @@
 import { handleGoogleSignUp, handleGitHubSignup } from '../../services/authService.js'
+import { handleRegisterEmail } from '../../services/userService.js'
 
 document.querySelector('.open-auth > div:first-child').onclick = handleGoogleSignUp
 document.querySelector('.open-auth > div:last-child').onclick = handleGitHubSignup
+
+const email = document.querySelector('#signup-container input[type="email"]')
+document.querySelector('#signup-container > form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    if (!email.checkValidity()) {
+        email.reportValidity()
+        return
+    }
+
+    handleRegisterEmail(email.value)
+})
