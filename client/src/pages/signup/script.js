b870925e9d8353c872b534e55eba010856cd8ff7
Vivaan Yadav <gravesoulkeeper@gmail.com>
2025-09-16 23:15:43 +0530

    remove log message

--- client/src/pages/signup/script.js
+++ client/src/pages/signup/script.js
@@ -1,13 +1,12 @@
-fetch('../../components/signupForm/index.html')
+fetch("../../components/signupForm/index.html")
     .then((signupForm) => signupForm.text())
     .then((signupForm) => {
-        document.getElementById('signup').innerHTML = signupForm
-        console.log('fetch successfully...')
-        const script = document.createElement('script')
-        script.type = 'module'
-        script.src = '../../components/signupForm/script.js'
-        document.body.appendChild(script)
+        document.getElementById("signup").innerHTML = signupForm;
+        const script = document.createElement("script");
+        script.type = "module";
+        script.src = "../../components/signupForm/script.js";
+        document.body.appendChild(script);
     })
     .catch((error) => {
-        console.error('Error while fetching signupForm \n', error)
-    })
+        console.error("Error while fetching signupForm \n", error);
+    });
